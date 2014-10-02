84d73c28841a2f7ac395e0d3aef1fb06f08ae485
simpleinformation <ali@simpleinformation.com>
2014-10-02 16:58:28 -0400

    calling regions in tpl.

--- sites/all/themes/v50theme/templates/page.tpl.php
+++ sites/all/themes/v50theme/templates/page.tpl.php
@@ -6,33 +6,96 @@
 
 
 
-<div id="zone-menu-wrapper" class="zone-wrapper zone-menu-wrapper clearfix">  
-  <div id="zone-menu" class="zone zone-menu clearfix container-12">
-    <div class="grid-9 region region-menu" id="region-menu">
-  <div class="region-inner region-menu-inner">
-        <nav class="navigation">
-      <h2 class="element-invisible">Main menu</h2><ul id="main-menu" class="links inline clearfix main-menu"><li class="menu-788 first"><a href="/node/2">Home</a></li><li class="menu-728"><a href="http://v50/summits" title="Link to the solutions summit section">Solutions Summits</a></li><li class="menu-779"><a href="http://home/join" title="">Join the Conversation</a></li><li class="menu-729"><a href="http://v50/incubator" title="Link to solutions incubator section">Solutions Incubator</a></li><li class="menu-778"><a href="http://home/organize" title="">Organize your Community</a></li><li class="menu-780 last"><a href="http://home/future" title="">$50 for the Future</a></li></ul>          </nav>
-        <div class="alpha-debug-block"><h2>Menu</h2><p>This is a debugging block</p></div>  </div>
-</div>
-  </div>
-</div>
-
-
-
 <?php if ($title): ?>
-	<div class="zone-wrapper zone-title-wrapper clearfix" id="zone-title-wrapper">  
-	<div class="zone zone-title clearfix container-12" id="zone-title">   
-     
 		
 <?php print render($title_prefix); ?> 	
 
+<div>
 
 
+<?php if (isset($page['sticky_menu'])) : ?>
+    <?php print render($page['sticky_menu']); ?>
+  <?php endif; ?>  
 
+<?php if (isset($page['social_share'])) : ?>
+    <?php print render($page['social_share']); ?>
+  <?php endif; ?>  
+
+  <?php if (isset($page['v50_video'])) : ?>
+    <?php print render($page['v50_video']); ?>
+  <?php endif; ?>  
+  
+
+<?php if (isset($page['soical_top'])) : ?>
+    <?php print render($page['soical_top']); ?>
+  <?php endif; ?>  
+
+<?php if (isset($page['soical_bottom'])) : ?>
+    <?php print render($page['soical_bottom']); ?>
+  <?php endif; ?>  
+
+<?php if (isset($page['summits_link'])) : ?>
+    <?php print render($page['summits_link']); ?>
+  <?php endif; ?>  
+
+  
+  <?php if (isset($page['solutions_summit'])) : ?>
+    <?php print render($page['solutions_summit']); ?>
+  <?php endif; ?>  
+
+ <?php if (isset($page['incubator_link'])) : ?>
+    <?php print render($page['incubator_link']); ?>
+  <?php endif; ?>  
+
+  <?php if (isset($page['incubator'])) : ?>
+    <?php print render($page['incubator']); ?>
+  <?php endif; ?>  
+  
+  
+
+<?php if (isset($page['showcase_link'])) : ?>
+    <?php print render($page['showcase_link']); ?>
+  <?php endif; ?>  
+
+<?php if (isset($page['showcase'])) : ?>
+    <?php print render($page['showcase']); ?>
+  <?php endif; ?>  
+
+  <?php if (isset($page['showcase_photos'])) : ?>
+    <?php print render($page['showcase_photos']); ?>
+  <?php endif; ?>  
+  
+
+ <?php if (isset($page['showcase_video'])) : ?>
+    <?php print render($page['showcase_video']); ?>
+  <?php endif; ?>  
+  
+  <?php if (isset($page['showcase_audio'])) : ?>
+    <?php print render($page['showcase_audio']); ?>
+  <?php endif; ?>  
+  
+<?php if (isset($page['showcase_posters'])) : ?>
+    <?php print render($page['showcase_posters']); ?>
+  <?php endif; ?>  
+  
+  
+ <?php if (isset($page['future'])) : ?>
+    <?php print render($page['future']); ?>
+  <?php endif; ?>  
    
   <?php if (isset($page['content'])) : ?>
     <?php print render($page['content']); ?>
   <?php endif; ?>  
+  
+<?php if (isset($page['community'])) : ?>
+    <?php print render($page['community']); ?>
+  <?php endif; ?>  
+
+ <?php if (isset($page['conversation'])) : ?>
+    <?php print render($page['conversation']); ?>
+  <?php endif; ?>  
+
+
   
   <?php if (isset($page['footer'])) : ?>
     <?php print render($page['footer']); ?>
@@ -51,21 +114,6 @@
 
 
 
- <?php if ($secondary_menu): ?>
-      <div id="secondary-menu" class="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'id' => 'secondary-menu-links',
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => t('Secondary menu'),
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </div> <!-- /#secondary-menu -->
-    <?php endif; ?>
+ 
 
 
