b730c7a375e5d8ccfd123611e8a5c00981e525bb
simpleinformation <ali@simpleinformation.com>
2014-10-15 11:36:15 -0400

    testing changes to past future area,removing footer social to sass

--- sites/all/themes/v50theme/templates/node.tpl.php
+++ sites/all/themes/v50theme/templates/node.tpl.php
@@ -1,8 +1,7 @@
 <div id="vgrid">
 <div class="page clearfix">
 <div class="v50vid_wrap">   
-<div class="v50video">
-<?php print render($content['field_v50_video']); ?></div>
+<div class="v50video"><?php print render($content['field_v50_video']); ?></div>
 <div style="clear:both;"></div>
 </div>
 
@@ -74,22 +73,24 @@
  	<div class="case_works"><?php print render($content['field_showcase_works']); ?></div>
   </div>
 
+
 <div class="past">past
 <div style="clear:both;"></div>
-<div class="past_title"><?php print render($content['field_past_title']); ?></div>
-<div style="clear:both;"></div>
+
+<div class="past_title">title<?php print render($content['field_past_title']); ?></div>
+
 
 <div class="photos_wrap">photo wrap
-  <div style="clear:both;"></div>
-<div class="photo_title"><?php print render($content['field_photo_title']); ?></div>
+<div style="clear:both;"></div>
+<div class="photo_title">title<?php print render($content['field_photo_title']); ?></div>
 <div style="clear:both;"></div>
 <div class="photos"><?php print render($content['field_past_photos']); ?>photos</div>
   <div style="clear:both;"></div>
 </div>
 
      	<div class="past_videos">past videos
-  <div style="clear:both;"></div>
-<div class="video_title"><?php print render($content['field_video_title']); ?></div>
+<div style="clear:both;"></div>
+<div class="video_title">title<?php print render($content['field_video_title']); ?></div>
 <div style="clear:both;"></div>
 		<div class="past_video1"><?php print render($content['field_past_video1']); ?>videos1</div>
  <div style="clear:both;"></div>
@@ -97,12 +98,12 @@
 		<div class="past_video3"><?php print render($content['field_past_video3']); ?>video3</div>
  <div style="clear:both;"></div>
  </div>
-
+ 
 
   
 <div class="audio_wrap">audio wrap
-  <div style="clear:both;"></div>
-<div class="audio_title"><?php print render($content['field_audio_title']); ?></div>
+<div style="clear:both;"></div>
+<div class="audio_title">title<?php print render($content['field_audio_title']); ?></div>
 <div style="clear:both;"></div>
 	
 	<div class="audio1"><?php print render($content['field_v50_audio']); ?>audio1</div>
@@ -120,7 +121,7 @@
 
 <div class="posters_wrap">posters wrap
 <div style="clear:both;"></div>
-<div class="poster_title"><?php print render($content['field_poster_title']); ?></div>
+<div class="poster_title">title<?php print render($content['field_poster_title']); ?></div>
 <div style="clear:both;"></div>
 <div class="posters"><?php print render($content['field_past_posters']); ?>posters</div>
   <div style="clear:both;"></div>
@@ -138,17 +139,12 @@
 
 <div class="footer_area">footer area
   <div style="clear:both;"></div>
-	<div class="footer_logo1">
- <div style="float: right;">
-<img alt="amerilogo" class="Image attr__field_file_image_alt_text[und][0][value]__americorp attr__field_file_image_title_text[und][0][value]__americorp attr__format__default img__fid__969 img__view_mode__default" src="sites/default/files/IMAGES/footer/americorps_footer.png" /></div>
+<div class="footer_logo1"></div>
+
+<div class="footer_logo2"></div>
+
+<div class="footer_menu">footer menu</div>
+<div class="social_bottom">social bottom</div>
+ </div>
 </div>
-
-     	<div class="footer_logo2">
-<img alt="ncs" class="Image attr__field_file_image_alt_text[und][0][value]__ncs attr__field_file_image_title_text[und][0][value]__ncs attr__format__default img__fid__969 img__view_mode__default" src="sites/default/files/IMAGES/footer/cncs_footer.png" /></div>
-
-
-	<div class="footer_menu">footer menu</div>
-     	<div class="social_bottom">social bottom
-<img alt="facebook" class="Image attr__field_file_image_alt_text[und][0][value]__facebook attr__field_file_image_title_text[und][0][value]__ attr__format__default img__fid__969 img__view_mode__default" src="sites/default/files/facebook_footer.png" /><img alt="twitter" class="Image attr__field_file_image_alt_text[und][0][value]__twitter attr__field_file_image_title_text[und][0][value]__ attr__format__default img__fid__970 img__view_mode__default" src="sites/default/files/twitter_footer.png" /><img alt="youtube" class="Image attr__field_file_image_alt_text[und][0][value]__youtube attr__field_file_image_title_text[und][0][value]__ attr__format__default img__fid__971 img__view_mode__default" src="sites/default/files/youtube_footer.png" /><img alt="flickr" class="Image attr__field_file_image_alt_text[und][0][value]__flickr attr__field_file_image_title_text[und][0][value]__ attr__format__default img__fid__972 img__view_mode__default" src="sites/default/files/flickr_footer.png" /><img alt="linkedin" class="Image attr__field_file_image_alt_text[und][0][value]__linkedin attr__field_file_image_title_text[und][0][value]__ attr__format__default img__fid__969 img__view_mode__default" src="sites/default/files/linkedin_footer.png" /></p></div>
-  </div>
 </div>
